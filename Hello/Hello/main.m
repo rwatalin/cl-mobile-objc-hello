0a7f445b1994bd2d4ca7d03d2240c167efb84304
rickykirkendall <rickykirkendall@flocoapps.com>
2016-01-18 20:39:30 -0500

    initial commit

---
+++ Hello/Hello/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello
+//
+//  Created by Ricky Kirkendall on 1/18/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert hello world here
+        
+    }
+    return 0;
+}
